9462ca81c3d36a1659fd23ba0e2271f7bc75ac12
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-07-17 22:37:00 +0200

    make the footroom start at 22 for bt709
    I don't know why this is needed but without this change the colors don't match
    with the ones mplayer2 is outputting.
    
    Please do contact me, if you are a color expert and know the answer.

--- lib/smart_ass/bt709_colormatrix.rb
+++ lib/smart_ass/bt709_colormatrix.rb
@@ -2,4 +2,8 @@ class SmartAss::BT709ColorMatrix < SmartAss::YCbCrColorMatrix
   def initialize
     super(0.2126, 0.7152, 0.0722)
   end
+
+  def offset_vector
+    Matrix.column_vector([22.0, 125.0, 125.0])
+  end
 end
